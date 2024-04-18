c591426a6c8959a715f75a3ad83570dbaaba4c0b
daniyal <63174165+Daniyal-Abdul-Rehman@users.noreply.github.com>
2024-04-18 15:36:45 +0500

    updates

--- src/components/atoms/Image.js
+++ src/components/atoms/Image.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 const ImageAtom = ({ src, alt ,width,height,className,onClick}) => {
-    return <Image src={src} alt={alt} width={width} height={height} className={className} onClick={onClick}/>;
+    return <img src={src} alt={alt} width={width} height={height} className={className} onClick={onClick}/>;
 };
 ImageAtom.defaultProps={
     width:30,
