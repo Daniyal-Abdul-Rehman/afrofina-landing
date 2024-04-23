41bc9771b12336b499423c5e6cb146e60c1b3af5
daniyal <63174165+Daniyal-Abdul-Rehman@users.noreply.github.com>
2024-04-24 00:30:33 +0500

    updates

--- src/components/organisms/Landing/pageOrgEight.js
+++ src/components/organisms/Landing/pageOrgEight.js
@@ -15,16 +15,18 @@ export default function PageOrgEight() {
 
     return (
         <div className='bg-black py-12 pb-24 animate-slide-in'>
-            <div className='flex lg:flex-row flex-col  items-center justify-between px-12 pb-12'>
-                <Text className="!text-primary lg:!text-[72px] !text-[28px] custom-font !tracking-widest lg:w-8/12 w-full" align="left">
+            <div className='flex lg:flex-row flex-col  items-center justify-between lg:px-12 px-5 pb-12'>
+                <Text className="!text-primary lg:!text-[72px] !text-[28px] custom-font !tracking-widest lg:w-8/12 w-full lg:!text-left !text-center" align="left">
                     <AnimatedElement>
                         Here is what people have to say about afrofina
                     </AnimatedElement>
                 </Text>
-                <Image src={'/landing/pageOrgEight/right.svg'} width={47} className={'swiper-button-next-landing cursor-pointer'} onClick={goNext} />
+                <div className='flex justify-end w-full lg:pb-0 pb-6'>
+                <Image src={'/landing/pageOrgEight/right.svg'} width={47} className={'swiper-button-next-landing cursor-pointer lg:!w-[47px] w-[24px]'} onClick={goNext} />
+                </div>
             </div>
             <AnimatedElement>
-                <div className='relative pl-12'>
+                <div className='relative lg:pl-12 pl-5'>
                     <Image src={'/landing/pageOrgEight/line.svg'} width={1280.5} className={'w-full pt-24 -mb-[180px] '} />
                     <div className=''>
                         <LandingReviewCarousel swiperRef={swiperRef} />
