804a4cf310df4631d86e797aa3b2a0a628c82acd
daniyal <63174165+Daniyal-Abdul-Rehman@users.noreply.github.com>
2024-04-24 18:05:43 +0500

    updates

--- src/components/organisms/Landing/PageOrgSeven.js
+++ src/components/organisms/Landing/PageOrgSeven.js
@@ -1,45 +1,54 @@
 'use client'
-import { useState } from 'react';
-import { AnimatedElement, Text, Image } from '@/components/atoms'
-import ReactPlayer from 'react-player'
-export default function PageOrgSeven({refTrack}) {
+import { useEffect, useState } from 'react';
+import { AnimatedElement, Text, Image } from '@/components/atoms';
+import ReactPlayer from 'react-player';
+
+export default function PageOrgSeven({ refTrack }) {
     const [isVideoPlaying, setIsVideoPlaying] = useState(true);
 
     const toggleVideo = () => {
-
-
         setIsVideoPlaying(!isVideoPlaying);
-
     };
+
+    useEffect(() => {
+        // Initially, video is playing
+        setIsVideoPlaying(true);
+
+        // After 3 seconds, pause the video
+        const timer = setTimeout(() => {
+            setIsVideoPlaying(true);
+        }, 3000);
+
+        // Clean up the timer to prevent memory leaks
+        return () => clearTimeout(timer);
+    }, []);
 
     return (
         <div ref={refTrack} className='pt-24 p-5 animate-slide-in'>
             <div className='flex justify-center'>
-
                 <Text className="lg:!text-[40px] !text-[20px] text-black custom-font !tracking-widest w-9/12 px-10 pb-12">
                     <AnimatedElement>
-                        Get the most out of afrofina, watch this video to know  how you can utilize the full potential of Afrofina.
-
+                        Get the most out of afrofina, watch this video to know how you can utilize the full potential of Afrofina.
                     </AnimatedElement>
                 </Text>
             </div>
             <AnimatedElement>
-                <div className={`lg:!h-[790px] !h-[254px] flex justify-center items-end lg:rounded-xl rounded-none overflow-hidden relative`} >
+                <div className={`lg:!h-[790px] !h-[254px] flex justify-center items-end lg:rounded-xl rounded-none overflow-hidden relative`}>
                     <div className='bg-transparentGray w-full h-full absolute top-0 left-0 z-10 flex justify-center items-center' onClick={toggleVideo}>
                         {!isVideoPlaying && <Image src="/play.svg" width={94} height={94} alt={'play'} />}
                     </div>
                     <ReactPlayer
                         url='/background.mp4'
+                        loop={true}
                         playing={isVideoPlaying}
-                        controls={true}
+                        controls={false}
                         className={`absolute top-0 left-0 w-full h-full z-0`}
                         width='100%'
                         height='100%'
                         style={{ objectFit: 'cover' }} // Ensure the video covers the entire div
                     />
                 </div>
-
             </AnimatedElement>
         </div>
-    )
+    );
 }
